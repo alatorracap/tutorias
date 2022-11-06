6488f72d58049331e3bedde87d7f178469125482
MarioCollarAlvarez <mariocollaralv@gmail.com>
2022-11-06 18:13:40 +0100

    Error management in login

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -1,13 +1,16 @@
 import { useState } from "react";
-import { Button, Form, Stack } from "react-bootstrap";
+import { Alert, Button, Form, Stack } from "react-bootstrap";
 import { Link, Navigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 
 import { useSetUser } from "../UserContext";
 import { userLogin, userLogout } from "../store";
+import { text } from "@fortawesome/fontawesome-svg-core";
 
 function Login(props) {
   const { setShow } = props;
+  const [showAlert, setShowAlert] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
   const dispatch = useDispatch();
 
   const [email, setEmail] = useState("");
@@ -16,7 +19,6 @@ function Login(props) {
 
   const handleLogin = async (e) => {
     // Call sethShow from parent(setShowLogin) function and set to false for close the modal
-    setShow(false);
 
     e.preventDefault();
     setStatus("loading");
@@ -31,59 +33,75 @@ function Login(props) {
           password,
         }),
       }
-    );
-    //* Mantiene la sesion guardada en el local storage
-    if (!res.ok) {
-      console.log("res", res);
-      // TODO: Manejar error
-    } else {
-      const data = await res.json();
-      console.log("data", data);
-      dispatch(userLogin(data));
-      localStorage.setItem("session", data.data.token);
-    }
+    )
+      .then((res) => {
+        if (!res.ok) {
+          return res.text().then((text) => {
+            setShowAlert(true);
+            setErrorMessage(text);
+            //throw new Error(text);
+          });
+        } else {
+          res.json().then((data) => {
+            setShow(false);
+            dispatch(userLogin(data));
+            localStorage.setItem("session", data.data.token);
+          });
+        }
+      })
+      .catch((err) => {
+        console.log("caught it!", err);
+      });
   };
 
   return (
-    <form onSubmit={handleLogin}>
-      <Form>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address {process.env.TECHNOLOGY} </Form.Label>
-          <Form.Control
-            type="email"
-            placeholder="Enter email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            name="email"
-          />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
-        </Form.Group>
+    <>
+      {showAlert && (
+        <Alert variant="danger" onClose={() => setShowAlert(false)} dismissible>
+          <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
+          <p>{errorMessage}</p>
+        </Alert>
+      )}
+      <form onSubmit={handleLogin}>
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address {process.env.TECHNOLOGY} </Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              name="email"
+            />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            name="password"
-          />
-        </Form.Group>
-        {status === "error" && (
-          <p className="error">Usuario o contraseña incorrectos.</p>
-        )}
-        <Stack direction="vertical" gap={2}>
-          <Button variant="primary" type="submit" onClick={handleLogin}>
-            Log in
-          </Button>
-          <p className="text-center , text-muted">
-            Please register if you haven't already done so.
-          </p>
-        </Stack>
-      </Form>
-    </form>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              name="password"
+            />
+          </Form.Group>
+          {status === "error" && (
+            <p className="error">Usuario o contraseña incorrectos.</p>
+          )}
+          <Stack direction="vertical" gap={2}>
+            <Button variant="primary" onClick={handleLogin}>
+              Log in
+            </Button>
+            <p className="text-center , text-muted">
+              Please register if you haven't already done so.
+            </p>
+          </Stack>
+        </Form>
+      </form>
+    </>
   );
 }
 export default Login;
